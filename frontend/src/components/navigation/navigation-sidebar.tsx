1fcf7ce3e3c43368c7924c6af42558ee7cddd9ad
Haoyang Guo <hyg.haoyang@gmail.com>
2023-09-26 20:03:37 -0700

    remove logo hover effect

--- frontend/src/components/navigation/navigation-sidebar.tsx
+++ frontend/src/components/navigation/navigation-sidebar.tsx
@@ -16,16 +16,7 @@ const NavigationSidebar = ({ servers }: { servers: ServerWithChannels[] }) => {
   const queryClient = useQueryClient();
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full bg-[#e3e5e8] dark:bg-[#1E1F22] py-3">
-      <ActionTooltip
-        side="right"
-        label={"Thanks for coming! I hope you like some oolong tea."}
-        align={"start"}
-      >
-        <button className="w-fit h-fit border hover:cursor-default">
-          <OolongLogo width={52} height={52} />
-        </button>
-      </ActionTooltip>
-
+      <OolongLogo width={52} height={52} />
       <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
       <ScrollArea className="flex-1 w-full">
         {servers?.map((server: any) => (
